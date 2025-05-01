0afc42cba361495ab6e8732a03d51282ebb8e8a4
ウエスリー <wesley@turnip.evokateur.net>
2025-05-01 00:06:06 -0700

    add util/database.js'

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const errorController = require('./controllers/error');
+const errorController = require('./controllers/error.js');
+const db = require('./util/database.js');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require('./routes/admin.js');
+const shopRoutes = require('./routes/shop.js');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
