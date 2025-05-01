080f047c6a990426525642727256944d3cd7c349
ウェスリー <wesley@sfizio.evokateur.net>
2025-05-01 13:59:28 -0700

    add table create sql for posterity, testing connection

---
+++ migrations/001_create_products.sql
@@ -0,0 +1,9 @@
+create table products (
+    id int unsigned not null auto_increment,
+    title varchar(255) not null,
+    price double not null,
+    description text not null,
+    imageUrl varchar(255) not null,
+    primary key (id),
+    unique index id_unique (id asc) visible
+);
